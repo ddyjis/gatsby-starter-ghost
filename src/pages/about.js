2aedd648c316758225826fc5879d01ef5e42f021
ddyjis <ddyjis@gmail.com>
2020-06-24 23:33:15 -0400

    Add about page

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Layout } from "../components/common";
+
+const AboutPage = () => (
+    <Layout>
+        <div className="container">
+            <article className="content" style={{ textAlign: `center` }}>
+                <h1 className="content-title">About</h1>
+                <section className="content-body">
+                    I am a software developer in Toronto. I write my notes here.
+                </section>
+            </article>
+        </div>
+    </Layout>
+);
+
+export default AboutPage;
